2a4227d1b0f31ced9e9b6bd890119edc404ff0e8
S L <rlunneborg1@gmail.com>
2026-07-22 11:58:45 -0600

    term 2.0 project

--- Plantor-aid_Midterm/src/Plantor-aid_Midterm.cpp
+++ Plantor-aid_Midterm/src/Plantor-aid_Midterm.cpp
@@ -2,22 +2,35 @@
 #include "Particle.h"
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_GFX.h"
+#include "Adafruit_BME280.h"
+//#include "Adafruit_MQTT_SPARK.h"
+//#include "Adafruit_MQTT.h"
+//#include "Credentials.h"
 
 const int OLED_RESET =-1;
 const int OLED_ADDRESS = 0x3C; //OLED display 
 
 const int PROBEPIN = A0;
-int rawData;
 int probReading;
 const int RELAYPIN = D4;
 
-int soilDry = 2840;
+int soilDry = 2700;
 int soilWet = 1414;
 int waterWet = 1616;
-int moistSoil;
+int tempF;
+int humidH;
+bool status;
+bool pumpOn = false;
+unsigned int lastPump = 0;  
+
+//TCPClient TheClient;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+//Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
 
 
 Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_BME280 bme;
 
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
@@ -40,39 +53,48 @@ display.setTextColor(WHITE);
 display.setTextSize(1);
 display.display();
 
+bme.begin(0x76);
+  Serial.printf("BME OK\n");
+
 
 }
 
-
-void loop() {
+void loop(){
   display.clearDisplay();
   display.setCursor(0,0); 
-
+ 
   probReading = analogRead(PROBEPIN);
-  Serial.printf("Moist Level: %i\n", probReading);
-  display.printf("Moist Level: %i\n", probReading);
+
+  tempF = bme.readTemperature();
+  tempF = tempF * (9.0/5.0)+32.0;
+  humidH = bme.readHumidity();
+
+  display.printf("Moist Level: %i\n\n", probReading);
+  display.printf("Temp:     %i\n\n", tempF);
+  display.printf("RH:       %i\n\n", humidH);
+  
   display.display();
-  delay(2000);
+  Serial.printf("Moist Level: %i\n", probReading);
+  
 
-  digitalWrite(RELAYPIN, HIGH);
-
-  if (moistSoil<1414){
-  digitalWrite(RELAYPIN, HIGH);
-  delay(3000);
-  digitalWrite(RELAYPIN, HIGH);
+if((millis() - lastPump) > 3000){
+  if (probReading> soilDry){
+    digitalWrite(RELAYPIN, HIGH);
+    delay(500);
+    digitalWrite(RELAYPIN, LOW);
   }
-
- else (moistSoil>2840);
-  digitalWrite(RELAYPIN, LOW);
-  delay(3000);
-
+  else{
+    digitalWrite(RELAYPIN, LOW);
+  
+  }
+ lastPump = millis();
+}
+  
+  
+  
+ 
 }
 
 
 
-//display.clearDisplay();
-//display.display();
-//delay(2000);
-
-  
 
